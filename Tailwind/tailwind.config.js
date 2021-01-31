33e171e7018975da857cd8eedca03013008458b9
Yerikho <l200160185.student.ums.ac.id>
2021-01-31 22:06:35 +0700

    Scriptnya di folder Tailwind kalo asset lengkapnya di folder asset mas

---
+++ Tailwind/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    objectPosition: {
+      bottom: 'bottom',
+      center: 'center',
+      left: 'left',
+      right: 'right',
+      top: 'top',
+    },
+
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
